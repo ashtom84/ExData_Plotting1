d6bb6c5d26a5a3d2d4dc94c1d1e80785ec655621
ashtom84 <thomas_boulenger@hotmail.fr>
2015-03-07 12:07:38 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-plot3 <- function(path = pathMac) {
+plot3 <- function(path = 'household_power_consumption.txt') {
   # Reading the file
   test <- read.csv(path, sep = ';')
   test$Date <- as.Date(test$Date, format = '%d/%m/%Y')
